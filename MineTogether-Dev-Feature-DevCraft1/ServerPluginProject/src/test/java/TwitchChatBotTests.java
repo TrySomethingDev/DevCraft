efddbf1be457b0be38529c9c5df26ca3eaf17322
TrySomethingDev <trysomethingdev@gmail.com>
2024-03-07 09:56:19 -0500

    Added JUnit5 Testing to build.gradle and created first example test.

---
+++ MineTogether-Dev-Feature-DevCraft1/ServerPluginProject/src/test/java/TwitchChatBotTests.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TwitchChatBotTests {
+
+    @Test
+    void firstTest(){
+
+        assertEquals(44, Integer.sum(19, 23));
+
+    }
+
+}
