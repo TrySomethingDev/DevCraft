2c3b8728fca2d5738e55b228fa89857e4e6ff78e
TrySomethingDev <trysomethingdev@gmail.com>
2024-03-09 15:11:58 -0500

    Removed Logging

--- MineTogether-Dev-Feature-DevCraft1/ServerPluginProject/src/main/java/net/trysomethingdev/twitchplugin/Utilites/Colorizer.java
+++ MineTogether-Dev-Feature-DevCraft1/ServerPluginProject/src/main/java/net/trysomethingdev/twitchplugin/Utilites/Colorizer.java
@@ -12,12 +12,12 @@ public class Colorizer {
     }
 
     public static void sendMessage(CommandSender sender, String message) {
-        Bukkit.getLogger().info("SendMessage Commander: " + sender.getName() + " " + message.toString());
+        //Bukkit.getLogger().info("SendMessage Commander: " + sender.getName() + " " + message.toString());
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
 
     public static void sendMessage(Player sender, String message) {
-        Bukkit.getLogger().info("SendMessage Player: " + sender.getName() + " " + message.toString());
+       // Bukkit.getLogger().info("SendMessage Player: " + sender.getName() + " " + message.toString());
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
 }
